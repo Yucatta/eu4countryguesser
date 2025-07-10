69099d60e7245317e36dd43cce0abcd05b921360
Yucatta <ahmetabali41@gmail.com>
2025-07-10 18:05:53 +0300

    test

--- src/Components/AllCountries.tsx
+++ src/Components/AllCountries.tsx
@@ -71,6 +71,7 @@ const AllCountries = ({
                   return prev;
                 });
               }}
+              // test
               countryclick={(bbox) => {
                 const xcord = bbox.x + bbox.width / 2;
                 const ycord = bbox.y + bbox.height / 2;
